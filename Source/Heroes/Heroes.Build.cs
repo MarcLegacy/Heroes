9a2251482e64a39708df4301b0f362058173e82d
Marc Meijering <marc_1997@live.nl>
2022-11-01 12:07:43 +0100

    Initial commit

---
+++ Source/Heroes/Heroes.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Heroes : ModuleRules
+{
+	public Heroes(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
